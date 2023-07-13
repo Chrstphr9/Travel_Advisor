7a526fee8f15202ab7fe05e4e3cf00025df6e23f
Chrstphr <somto565@gmail.com>
2023-07-13 11:38:19 +0200

    working on header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
             <Header/>
             <Grid container spacing={3} style={{ width:"100%" }}>
                 <Grid item xs={12} md={4}> <List /> </Grid>
-                <Grid item xs={12} md={4}> <Map/> </Grid>
+                <Grid item xs={12} md={8}> <Map/> </Grid>
             </Grid>
         </>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@mui/material";
+import { Autocomplete } from "@react-google-maps/api";
+import React from "react";
+import SearchIcon from "@mui/icons-material";
 
 const Header = () => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor 
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore New Places
+          </Typography>
+          <Autocomplete>
+            <div className={classes.serach}>
+              <div className={classes.searchIcon}></div>
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
